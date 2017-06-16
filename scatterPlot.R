c714f6cdc120d3842c70f4d2a274c6eb15862b7d
Yan Gao <yangao07@hit.edu.cn>
2017-06-15 20:44:26 -0700

    fix legend

--- scatterPlot.R
+++ scatterPlot.R
@@ -7,15 +7,15 @@ data<-read.table(argv[1], header=T, check.names=F)
 
 whole=400000
 x<-colnames(data)
-Accuracy <- as.numeric(data[4,] / (whole-data[1,]))
-Precision <- as.numeric(data[4,] / whole)
-all <- data.frame(x, Accuracy, Precision)
+Precision <- as.numeric(data[4,] / (whole-data[1,]))
+Recall <- as.numeric(data[4,] / whole)
+all <- data.frame(x, Precision, Recall)
 all_d <- melt(all, id = "x")
 ggplot(all_d, aes(x=x, y=value, group=variable, color=variable))  +
     geom_point(aes(shape=variable)) + 
     geom_line() + ## aes(linetype=variable), size=1
     xlab("MEM length") + 
-    ylab("Accuracy and Precision") +        
+    ylab("Precision and Recall") +        
     theme(legend.position = c(0.8, 0.50)) +  ## position of legend (0,0~1,1)
     theme(legend.title=element_blank())      ## remove legend title
 
